b7770db4c5d9b5c3ffd74c1850f714f2a4c30997
Dhiego Gonçalves da Silva <dhhiego@gmail.com>
2022-09-01 16:45:40 -0400

    Implementando os controllers Book e Loan.

--- src/main/java/br/com/americanas/estagiotech/libraryapi/LibraryApiApplication.java
+++ src/main/java/br/com/americanas/estagiotech/libraryapi/LibraryApiApplication.java
@@ -2,12 +2,18 @@ package br.com.americanas.estagiotech.libraryapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(info = @Info(title = "Library API", version = "1.0", description = "Api de aluguel de livros"))
+@EnableScheduling
 @SpringBootApplication
 public class LibraryApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LibraryApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApiApplication.class, args);
+    }
 
 }
