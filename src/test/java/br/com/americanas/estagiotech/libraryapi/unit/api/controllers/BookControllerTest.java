79259af2cdf4e6b19ee7dcbe8761d22f44743d38
Dhiego Gonçalves da Silva <dhhiego@gmail.com>
2022-09-13 23:33:57 -0400

    Criando mais testes unitários.

--- src/test/java/br/com/americanas/estagiotech/libraryapi/unit/api/controllers/BookControllerTest.java
+++ src/test/java/br/com/americanas/estagiotech/libraryapi/unit/api/controllers/BookControllerTest.java
@@ -1,6 +1,9 @@
 package br.com.americanas.estagiotech.libraryapi.unit.api.controllers;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.BDDMockito;
@@ -19,6 +22,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import br.com.americanas.estagiotech.libraryapi.api.controllers.BookController;
 import br.com.americanas.estagiotech.libraryapi.api.dtos.book.CreateBookRequest;
+import br.com.americanas.estagiotech.libraryapi.api.dtos.book.UpdateBookRequest;
+import br.com.americanas.estagiotech.libraryapi.api.exceptions.BusinessException;
 import br.com.americanas.estagiotech.libraryapi.models.Book;
 import br.com.americanas.estagiotech.libraryapi.services.BookService;
 import br.com.americanas.estagiotech.libraryapi.services.LoanService;
@@ -44,8 +49,14 @@ public class BookControllerTest {
     @MockBean
     private LoanService loanService;
 
+    @BeforeEach
+    void cleanUp() {
+        Mockito.reset(bookService, loanService);
+
+    }
+
     @Test
-    void givenAValidBook_whenCallsCreateBook_thenReturnBook() throws Exception {
+    public void givenValidBook_whenCallsCreateBook_thenReturnBookCreated() throws Exception {
         var expectedId = 1L;
         var expectedTitle = "Clean Code";
         var expectedIsbn = "9780132350884";
@@ -73,7 +84,7 @@ public class BookControllerTest {
         var json = mapper.writeValueAsString(createBookRequest);
 
         var request = MockMvcRequestBuilders.post(BOOK_API_URL)
-                .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON).content(json);
+                .contentType(MediaType.APPLICATION_JSON).content(json);
 
         mockMvc.perform(request).andExpect(MockMvcResultMatchers.status().isCreated())
                 .andExpect(MockMvcResultMatchers.jsonPath("id").value(expectedId))
@@ -86,6 +97,88 @@ public class BookControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("updated_at").isNotEmpty());
 
         Mockito.verify(bookService, Mockito.times(1)).create(bookCaptor.capture());
+
+        var book = bookCaptor.getValue();
+
+        Assertions.assertEquals(expectedTitle, book.getTitle());
+        Assertions.assertEquals(expectedIsbn, book.getIsbn());
+        Assertions.assertEquals(expectedAuthor, book.getAuthor());
+        Assertions.assertEquals(expectedEdition, book.getEdition());
+        Assertions.assertEquals(expectedPublisher, book.getPublisher());
+    }
+
+    @Test
+    public void givenExistingBookIsbn_whenCallsCreateBook_thenReturnStatus400() throws Exception {
+
+        var createBookRequest = CreateBookRequest.builder()
+                .title("Clean Code")
+                .isbn("9780132350884")
+                .author("Robert C. Martin")
+                .edition(1)
+                .publisher("Pearson").build();
+
+        BDDMockito.given(bookService.create(Mockito.any())).willThrow(new BusinessException("Isbn já foi cadastrado"));
+
+        var json = mapper.writeValueAsString(createBookRequest);
+
+        var request = MockMvcRequestBuilders.post(BOOK_API_URL)
+                .contentType(MediaType.APPLICATION_JSON).content(json);
+
+        mockMvc.perform(request).andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("status").value("BAD_REQUEST"))
+                .andExpect(MockMvcResultMatchers.jsonPath("message").value("Isbn já foi cadastrado"));
+    }
+
+    @Test
+    public void givenValidBook_whenCallsUpdateBook_thenReturnBookUpdated() throws Exception {
+        var expectedId = 1L;
+        var expectedTitle = "Clean Code";
+        var expectedIsbn = "9780132350884";
+        var expectedAuthor = "Robert C. Martin";
+        var expectedEdition = 1;
+        var expectedPublisher = "Pearson";
+
+        var updateBookRequest = UpdateBookRequest.builder()
+                .title(expectedTitle)
+                .author(expectedAuthor)
+                .edition(expectedEdition)
+                .publisher(expectedPublisher).build();
+
+        var bookFound = Book.create(
+                "Clean",
+                expectedIsbn,
+                "Robert",
+                2,
+                "P");
+        bookFound.setId(expectedId);
+
+        var updatedBook = Book.create(
+                expectedTitle,
+                expectedIsbn,
+                expectedAuthor,
+                expectedEdition,
+                expectedPublisher);
+        updatedBook.setId(expectedId);
+
+        BDDMockito.given(bookService.getById(expectedId)).willReturn(Optional.of(bookFound));
+        BDDMockito.given(bookService.update(Mockito.any())).willReturn(updatedBook);
+
+        var json = mapper.writeValueAsString(updateBookRequest);
+
+        var request = MockMvcRequestBuilders.put(BOOK_API_URL + "/" + expectedId)
+                .contentType(MediaType.APPLICATION_JSON).content(json);
+
+        mockMvc.perform(request).andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("id").value(expectedId))
+                .andExpect(MockMvcResultMatchers.jsonPath("title").value(expectedTitle))
+                .andExpect(MockMvcResultMatchers.jsonPath("isbn").value(expectedIsbn))
+                .andExpect(MockMvcResultMatchers.jsonPath("author").value(expectedAuthor))
+                .andExpect(MockMvcResultMatchers.jsonPath("edition").value(expectedEdition))
+                .andExpect(MockMvcResultMatchers.jsonPath("publisher").value(expectedPublisher))
+                .andExpect(MockMvcResultMatchers.jsonPath("created_at").isNotEmpty())
+                .andExpect(MockMvcResultMatchers.jsonPath("updated_at").isNotEmpty());
+
+        Mockito.verify(bookService, Mockito.times(1)).update(bookCaptor.capture());
 
         var book = bookCaptor.getValue();
 
--- src/test/java/br/com/americanas/estagiotech/libraryapi/unit/services/BookServiceTest.java
+++ src/test/java/br/com/americanas/estagiotech/libraryapi/unit/services/BookServiceTest.java
@@ -1,18 +1,23 @@
 package br.com.americanas.estagiotech.libraryapi.unit.services;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.test.context.ActiveProfiles;
 
 import br.com.americanas.estagiotech.libraryapi.api.exceptions.BusinessException;
@@ -31,9 +36,6 @@ public class BookServiceTest {
     @Mock
     private LoanRepository loanRepository;
 
-    @Captor
-    private ArgumentCaptor<Book> bookCaptor;
-
     @InjectMocks
     private BookService bookService;
 
@@ -43,7 +45,68 @@ public class BookServiceTest {
     }
 
     @Test
-    public void givenAValidBookId_whenCallsGetById_thenReturnBook() {
+    public void givenValidParams_whenCallsFindAll_thenReturnBooks() {
+        // given
+        var expectedId = 1L;
+        var expectedTitle = "Clean Code";
+        var expectedIsbn = "9780132350884";
+        var expectedAuthor = "Robert C. Martin";
+        var expectedEdition = 1;
+        var expectedPublisher = "Pearson";
+
+        var expectedPage = 0;
+        var expectedPerPage = 10;
+        var expectedTotal = 1;
+
+        var bookResult = Book.create(
+                expectedTitle,
+                expectedIsbn,
+                expectedAuthor,
+                expectedEdition,
+                expectedPublisher);
+        bookResult.setId(expectedId);
+
+        var bookFilter = Book.filter(
+                expectedTitle,
+                null,
+                null,
+                null,
+                null);
+
+        var bookExample = Example.of(bookFilter, ExampleMatcher.matching()
+                .withIgnoreCase()
+                .withIgnoreNullValues()
+                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING));
+
+        var pageRequest = PageRequest.of(
+                expectedPage, expectedPerPage,
+                Sort.by(Direction.fromString("asc"), "title"));
+
+        var page = new PageImpl<Book>(List.of(bookResult), pageRequest, expectedTotal);
+
+        Mockito.when(bookRepository.findAll(bookExample, pageRequest))
+                .thenReturn(page);
+
+        // when
+        var pageResult = bookService.findAll(bookFilter, pageRequest);
+
+        // then
+        Assertions.assertEquals(expectedPage, pageResult.getNumber());
+        Assertions.assertEquals(expectedPerPage, pageResult.getSize());
+        Assertions.assertEquals(expectedTotal, pageResult.getTotalElements());
+        Assertions.assertEquals(expectedId, pageResult.getContent().get(0).getId());
+        Assertions.assertEquals(expectedTitle, pageResult.getContent().get(0).getTitle());
+        Assertions.assertEquals(expectedIsbn, pageResult.getContent().get(0).getIsbn());
+        Assertions.assertEquals(expectedAuthor, pageResult.getContent().get(0).getAuthor());
+        Assertions.assertEquals(expectedEdition, pageResult.getContent().get(0).getEdition());
+        Assertions.assertEquals(expectedPublisher, pageResult.getContent().get(0).getPublisher());
+        Assertions.assertNotNull(pageResult.getContent().get(0).getCreatedAt());
+        Assertions.assertNotNull(pageResult.getContent().get(0).getUpdatedAt());
+    }
+
+    @Test
+    public void givenValidId_whenCallsGetById_thenReturnBook() {
+        // given
         var expectedId = 1L;
         var expectedTitle = "Clean Code";
         var expectedIsbn = "9780132350884";
@@ -66,8 +129,10 @@ public class BookServiceTest {
         Mockito.when(bookRepository.findById(expectedId))
                 .thenReturn(Optional.of(bookResult));
 
+        // when
         var book = bookService.getById(expectedId);
 
+        // then
         Assertions.assertEquals(expectedId, book.get().getId());
         Assertions.assertEquals(expectedTitle, book.get().getTitle());
         Assertions.assertEquals(expectedIsbn, book.get().getIsbn());
@@ -79,18 +144,22 @@ public class BookServiceTest {
     }
 
     @Test
-    public void givenAnInvalidBookId_whenCallsGetById_thenReturnEmptyOptional() {
+    public void givenInvalidId_whenCallsGetById_thenReturnEmptyOptional() {
+        // given
         var id = 1L;
 
         Mockito.when(bookRepository.findById(id)).thenReturn(Optional.empty());
 
+        // when
         var book = bookService.getById(id);
 
+        // then
         Assertions.assertFalse(book.isPresent());
     }
 
     @Test
-    public void givenAValidBook_whenCallsCreateBook_thenReturnBook() {
+    public void givenValidBook_whenCallsCreateBook_thenReturnBookCreated() {
+        // given
         var expectedId = 1L;
         var expectedTitle = "Clean Code";
         var expectedIsbn = "9780132350884";
@@ -116,8 +185,10 @@ public class BookServiceTest {
         Mockito.when(bookRepository.existsByIsbn(expectedIsbn)).thenReturn(false);
         Mockito.when(bookRepository.save(book)).thenReturn(bookResult);
 
+        // when
         var bookCreated = bookService.create(book);
 
+        // then
         Assertions.assertEquals(expectedId, bookCreated.getId());
         Assertions.assertEquals(expectedTitle, bookCreated.getTitle());
         Assertions.assertEquals(expectedIsbn, bookCreated.getIsbn());
@@ -129,7 +200,8 @@ public class BookServiceTest {
     }
 
     @Test
-    public void givenAnExistingBookIsbn_whenCallsCreateBook_thenReturnBusinessException() {
+    public void givenExistingBookIsbn_whenCallsCreateBook_thenReturnBusinessException() {
+        // given
         var expectedErrorMessage = "Isbn já foi cadastrado";
 
         var book = Book.create(
@@ -141,15 +213,18 @@ public class BookServiceTest {
 
         Mockito.when(bookRepository.existsByIsbn(book.getIsbn())).thenReturn(true);
 
+        // when
         var exception = Assertions.assertThrows(BusinessException.class, () -> bookService.create(book));
 
+        // then
         Assertions.assertEquals(expectedErrorMessage, exception.getMessage());
 
         Mockito.verify(bookRepository, Mockito.times(0)).save(book);
     }
 
     @Test
-    public void givenAValidBook_whenCallsUpdateBook_thenReturnBook() {
+    public void givenValidBook_whenCallsUpdateBook_thenReturnBookUpdated() {
+        // given
         var expectedId = 1L;
         var expectedTitle = "Clean Code";
         var expectedIsbn = "9780132350884";
@@ -177,6 +252,7 @@ public class BookServiceTest {
 
         Mockito.when(bookRepository.save(book)).thenReturn(bookResult);
 
+        // then
         var bookUpdated = bookService.update(book);
 
         Assertions.assertEquals(expectedId, bookUpdated.getId());
@@ -190,7 +266,8 @@ public class BookServiceTest {
     }
 
     @Test
-    public void givenAValidBook_whenCallsDeleteBook_thenDontReturnException() {
+    public void givenValidBook_whenCallsDeleteBook_thenDontReturnException() {
+        // given
         var book = Book.create(
                 "Clean Code",
                 "9780132350884",
@@ -201,13 +278,16 @@ public class BookServiceTest {
 
         Mockito.when(loanRepository.existsByBookAndActiveTrue(book)).thenReturn(false);
 
+        // when
         Assertions.assertDoesNotThrow(() -> bookService.delete(book));
 
+        // then
         Mockito.verify(bookRepository, Mockito.times(1)).delete(book);
     }
 
     @Test
-    public void givenAValidBookWithActiveLoan_whenCallsDeleteBook_thenReturnException() {
+    public void givenValidBookWithActiveLoan_whenCallsDeleteBook_thenReturnBusinessException() {
+        // given
         var expectedErrorMessage = "Não é possivel deletar um livro com empréstimo ativo";
 
         var book = Book.create(
@@ -220,8 +300,10 @@ public class BookServiceTest {
 
         Mockito.when(loanRepository.existsByBookAndActiveTrue(book)).thenReturn(true);
 
+        // when
         var exception = Assertions.assertThrows(BusinessException.class, () -> bookService.delete(book));
 
+        // then
         Assertions.assertEquals(expectedErrorMessage, exception.getMessage());
 
         Mockito.verify(bookRepository, Mockito.times(0)).delete(book);
